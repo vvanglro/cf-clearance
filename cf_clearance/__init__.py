aef125366c4a510b17272a25f73499978d9a5a7a
vvanglro <947001731@qq.com>
2022-11-23 14:44:12 +0800

    version: v1.28.0

--- cf_clearance/__init__.py
+++ cf_clearance/__init__.py
@@ -1,7 +1,7 @@
 from cf_clearance.stealth import async_stealth, sync_stealth, StealthConfig
 from cf_clearance.retry import async_cf_retry, sync_cf_retry
 
-__version__ = "1.27.1"
+__version__ = "1.28.0"
 
 __all__ = (
     "async_stealth",
