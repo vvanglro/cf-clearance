48eeee6a3a0ef7a2dd1a11cc425dc15f67811e74
KohnoseLami <kohnoselami@gmail.com>
2022-11-24 09:40:08 +0900

    Update retry.py

--- cf_clearance/retry.py
+++ cf_clearance/retry.py
@@ -12,46 +12,22 @@ from cf_clearance.errors import RecaptchaChallengeException
 async def async_cf_retry(page: AsyncPage, tries=10) -> bool:
     success = False
     while tries != 0:
-        try:
-            title = await page.title()
-        except Error:
+        if await page.query_selector('#challenge-form'):
             tries -= 1
-            await asyncio.sleep(1)
+            await page.wait_for_timeout(1000)
         else:
-            if title == 'Please Wait... | Cloudflare':
-                raise RecaptchaChallengeException("Encountered recaptcha. Check whether your proxy is an elite proxy.")
-            elif title == 'Just a moment...':
-                tries -= 1
-                await asyncio.sleep(2)
-            elif "www." in title:
-                await page.reload()
-                tries -= 1
-                await asyncio.sleep(3)
-            else:
-                success = True
-                break
+            success = True
+            break
     return success
 
 
 def sync_cf_retry(page: SyncPage, tries=10) -> bool:
     success = False
     while tries != 0:
-        try:
-            title = page.title()
-        except Error:
+        if page.query_selector('#challenge-form'):
             tries -= 1
-            time.sleep(1)
+            page.wait_for_timeout(1000)
         else:
-            if title == 'Please Wait... | Cloudflare':
-                raise RecaptchaChallengeException("Encountered recaptcha. Check whether your proxy is an elite proxy.")
-            elif title == 'Just a moment...':
-                tries -= 1
-                time.sleep(2)
-            elif "www." in title:
-                page.reload()
-                tries -= 1
-                time.sleep(3)
-            else:
-                success = True
-                break
+            success = True
+            break
     return success
