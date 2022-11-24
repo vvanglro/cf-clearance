b787493b79b3c337943d1176fc5009b276a054ec
vvanglro <947001731@qq.com>
2022-11-24 10:39:28 +0800

    feat: increase some waiting time.

--- cf_clearance/retry.py
+++ cf_clearance/retry.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-import asyncio
-import time
-
 from playwright.async_api import Error
 from playwright.async_api import Page as AsyncPage
 from playwright.sync_api import Page as SyncPage
 
-from cf_clearance.errors import RecaptchaChallengeException
 
-
-async def async_cf_retry(page: AsyncPage, tries=10) -> bool:
+async def async_cf_retry(page: AsyncPage, tries: int = 10) -> bool:
+    success = False
     while tries != 0:
         try:
             success = (False if await page.query_selector('#challenge-form') else True)
@@ -19,11 +15,12 @@ async def async_cf_retry(page: AsyncPage, tries=10) -> bool:
             break
         else:
             tries -= 1
-            await page.wait_for_timeout(1000)
+            await page.wait_for_timeout(1500)
     return success
 
 
-def sync_cf_retry(page: SyncPage, tries=10) -> bool:
+def sync_cf_retry(page: SyncPage, tries: int = 10) -> bool:
+    success = False
     while tries != 0:
         try:
             success = (False if page.query_selector('#challenge-form') else True)
@@ -33,5 +30,5 @@ def sync_cf_retry(page: SyncPage, tries=10) -> bool:
             break
         else:
             tries -= 1
-            page.wait_for_timeout(1000)
+            page.wait_for_timeout(1500)
     return success
