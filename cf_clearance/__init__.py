f5d36a17c3784e7c3be971a324a8dfa3f0478e94
vvanglro <vvanglro@gmail.com>
2023-07-13 11:39:07 +0800

    version: 0.30.0
    Signed-off-by: vvanglro <vvanglro@gmail.com>

--- cf_clearance/__init__.py
+++ cf_clearance/__init__.py
@@ -1,7 +1,8 @@
 from cf_clearance.retry import async_cf_retry, sync_cf_retry
 from cf_clearance.stealth import StealthConfig, async_stealth, sync_stealth
+from importlib.metadata import version
 
-__version__ = "0.29.2"
+__version__ = version("cf_clearance")
 
 __all__ = (
     "async_stealth",
